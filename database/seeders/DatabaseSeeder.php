a9116e46fd08fb3871014340679a091a17f76dd0
ReynaldiRP <rizkypratama471@gmail.com>
2025-09-02 09:18:24 +0700

    feat: add roles table for the user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,10 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        
+        Role::factory()->count(2)->create();
         User::factory()->create([
+            'role_id' => 1,
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
         ]);
+
     }
 }
